607fc465c9c59b1e62b2b92e18c809c8577edef5
ynqa <un.pensiero.vano@gmail.com>
2018-10-01 02:46:48 +0900

    Fix

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t ynqa/tensorflow-servimg-exporter:latest
-docker push ynqa/tensorflow-servimg-exporter:latest
+docker build -t ynqa/tensorflow-serving-exporter:latest .
+docker push ynqa/tensorflow-serving-exporter:latest
